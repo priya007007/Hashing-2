3e71cbdced2e6cb2675c0bd8713fd5b661f0e8a8
Priya <raravind@usc.edu>
2022-01-08 19:33:25 +0400

    Testrun

--- Sample.java
+++ Sample.java
@@ -5,3 +5,16 @@
 
 
 // Your code here along with comments explaining your approach
+// class Solution:
+//     def longestPalindrome(self, s: str) -> int:
+//         k = set()
+//         res= 0
+//         for i in range(len(s)):
+//             if s[i] in k:
+//                 k.remove(s[i])
+//                 res = res + 2
+//             else: 
+//                 k.add(s[i])
+//         if len(k)>0:
+//             res = res + 1
+//         return res
